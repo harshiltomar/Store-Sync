bb9e21bb7ec7313f7b1f6b9b6eaa6258019bd584
HarshilTomar20 <harshiltomar20@gmail.com>
2024-08-05 12:38:25 +0530

    chore: ui fix, redux Dark

--- client/src/app/dashboard-wrapper.tsx
+++ client/src/app/dashboard-wrapper.tsx
@@ -2,13 +2,32 @@
 
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
-import StoreProvider from "./redux";
+import StoreProvider, { useAppSelector } from "./redux";
+import { useEffect } from "react";
 
 function DashboardLayout({children}: {children: React.ReactNode}) {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const isDarkMode = useAppSelector(
+    (state) => state.global.isDarkMode
+  )
+
+  useEffect(()=> {
+    if (isDarkMode ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  })
+
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div className={`${isDarkMode ? 'dark' : 'light' } flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar/>
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200 md:pl-24`}>
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200 ${
+        isSidebarCollapsed ? 'pl-24' : 'pl-72'}`
+      }>
         <Navbar/>
         {children}
       </main> 
